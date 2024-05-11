ed30de65acf1d92bce299a55196b13d09450f1e7
apaolaoliveira <paolaoliveira.dev@gmail.com>
2024-05-10 23:47:48 -0300

    sty card

--- src/javascript/GiphyModule/GiphyView.js
+++ src/javascript/GiphyModule/GiphyView.js
@@ -4,13 +4,13 @@ export class GiphyView extends GiphyService {
   constructor(root){
     super(root);  
     this.onSearchClick();  
-    this.trendingGifs(); 
+    // this.trendingGifs(); 
   }
 
   onSearchClick(){
     this.root.querySelector('#search-btn').onclick = () => {
       const { value } = this.root.querySelector('#search-input');
-      this.search(value);
+      this.searchGif(value);
     }
   }
 }
